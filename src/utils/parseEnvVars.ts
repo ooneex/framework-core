20a01983fe70b82ff65c3f2eb2123f28c423b68e
Franck <fkdiomande@icloud.com>
2025-04-30 13:20:43 +0300

    Update parseEnvVars to register env vars in dependency container

--- src/utils/parseEnvVars.ts
+++ src/utils/parseEnvVars.ts
@@ -1,11 +1,16 @@
-import { parseString, toCamelCase } from '@';
+import { container } from '../container';
 import type { ScalarType } from '../types';
+import { parseString } from './parseString';
+import { toCamelCase } from './toCamelCase';
 
 export const parseEnvVars = <T = Record<string, ScalarType>>(): T => {
   const vars: Record<string, ScalarType> = {};
 
   for (const key in Bun.env) {
-    vars[toCamelCase(key)] = parseString(Bun.env[key] as string);
+    const k = toCamelCase(key);
+    const value = parseString(Bun.env[key] as string) as ScalarType;
+    container.bind(`env.${k}`).toConstantValue(value);
+    vars[k] = value;
   }
 
   return vars as T;
